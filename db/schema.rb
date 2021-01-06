9d822066a55265813f03f061038a9d6fb9d78b1d
claudiahalip <claudia4ro@yahoo.com>
2021-01-06 12:58:08 -0600

    validates added for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_045236) do
+ActiveRecord::Schema.define(version: 2021_01_06_184413) do
 
   create_table "photographers", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2020_09_27_045236) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["photographer_id"], name: "index_reviews_on_photographer_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "reviews", "photographers"
